c98698f83ee25cfd4c334493a94013b4e14c41fa
UncleVesimir <jmzmelling@gmail.com>
2017-10-27 19:20:06 +0100

    Add Song List component

--- client/index.js
+++ client/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
-import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider } from 'react-apollo'; //Injects code from Apollo store into React app
+const client = new ApolloClient({}); // Acts as a client side store that for all information coming from graphql
+// assumes backend graphql server is available at '/graphql'
 
-const client = new ApolloClient({}); // assumes backend graphql server is available at '/graphql'
-
+import SongList from './components/SongList'
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-        <div>Lyrical</div>
+        <SongList />
       </ApolloProvider>
   )
 };
