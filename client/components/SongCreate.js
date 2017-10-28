221dc600ae9d83dbedca9debc4c4512201270c72
UncleVesimir <jmzmelling@gmail.com>
2017-10-29 00:29:05 +0100

    Add SongCreate submission mutation and redirect to song list on submission

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo'
 
 class SongCreate extends Component {
   constructor(props){
@@ -6,11 +8,23 @@ class SongCreate extends Component {
     this.state = {title: ""};
   }
 
+  onSubmit = (event) => {
+    console.log(this.props)
+    event.preventDefault();
+    console.log(event);
+    this.props.mutate({
+      variables: {
+        title: this.state.title
+      }
+    })
+    this.props.history.push('/');
+  }
+
   render(){
     return (
       <div>
           <h3> Create a new song</h3>
-        <form>
+        <form onSubmit={this.onSubmit}>
           <label> Song Title:</label>
           <input 
           onChange={event => this.setState({title : event.target.value})}
@@ -22,4 +36,13 @@ class SongCreate extends Component {
   }
 }
 
-export default SongCreate;
+const mutation = gql`
+mutation AddSong($title: String){
+  addSong(title:$title){
+    title
+  }
+}
+`;
+
+
+export default graphql(mutation)(SongCreate);
